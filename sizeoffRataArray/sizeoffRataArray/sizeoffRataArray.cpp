7d6349a646a14d8463a1132e7186e5bd2f7ebc5e
Rv24 <rifqiahd@gmail.com>
2024-06-17 23:13:53 +0700

    add file #2

---
+++ sizeoffRataArray/sizeoffRataArray/sizeoffRataArray.cpp
@@ -0,0 +1,50 @@
+// sizeoffRataArray.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+// int k,sum,p;
+// void rata(){
+//     k=sum/p;  
+// }
+
+
+int main() {
+    // membuat array kosong
+    int i, p;
+    int nilai[] = { 23,45,21,45,11,99 };
+    float rata;
+    // mengisi array
+    // nilai[0] = 32;
+    // nilai[1] = 42;
+    // nilai[2] = 76;
+    // nilai[3] = 31;
+    // nilai[4] = 57;
+
+    p = sizeof(nilai) / sizeof(nilai[0]);
+    // mencetak isi array dengan perulangan
+    for (int i = 0; i < p; i++) {
+        cout << "Nilai ke-" << i << " :" << nilai[i] << endl;
+    }
+    cout << "\npanjang data :" << p;
+
+    for (int i = 0; i < p; i++) {
+        int sum += nilai[i];
+    }
+    cout << "\nJumlah :" << sum;
+    rata = (float)sum / (float)p;
+    cout << "\nMean :" << rata;
+
+    // rata();
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
